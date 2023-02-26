6343fe5adccd194295bba322eb5d9a41d9206859
dayarasilkey <silkeydayararamosguerra@gmail.com>
2023-02-26 18:52:18 -0500

    cafespros

--- Proyecto IntelliJ/cafe_demo_intellij/cafe_demo_intellij/src/main/java/com/proyect_cafeteria_intellij/cafe_demo_intellij/Controlador/GestionCaja.java
+++ Proyecto IntelliJ/cafe_demo_intellij/cafe_demo_intellij/src/main/java/com/proyect_cafeteria_intellij/cafe_demo_intellij/Controlador/GestionCaja.java
@@ -1,4 +1,25 @@
 package com.proyect_cafeteria_intellij.cafe_demo_intellij.Controlador;
 
+import com.proyect_cafeteria_intellij.cafe_demo_intellij.Modelo.Cafe;
+
+import java.util.ArrayList;
+
 public class GestionCaja {
+
+    ArrayList<Cafe> cafesCitos = new ArrayList<>();
+    public void CrearCafes() {
+        Cafe cafecito1 = new Cafe("abc111", "Mocha Blanco Latte", "Mochas", "Espresso 100% colombiano con leche vaporizada, salsa de mocha blanco", 8000);
+        Cafe cafecito2 = new Cafe("abc112", "Latte", "Latte", "Espresso colombiano con leche vaporizada, ligeramente cubierto con espuma.", 9000);
+        Cafe cafecito3 = new Cafe("abc113", "Dolce Canela Latte", "Latte", "Espresso colombiano con leche vaporizada, jarabe de canela dulce.", 6000);
+        Cafe cafecito4 = new Cafe("abc114", "Arequipe Latte", "Latte", "Espresso 100% colombiano con leche vaporizada a la perfección, combinado con salsa de arequipe, terminado con espiral de caramelo.", 7000);
+
+
+        cafesCitos.add(cafecito1);
+        cafesCitos.add(cafecito2);
+        cafesCitos.add(cafecito3);
+        cafesCitos.add(cafecito4);
+
+
+    }
 }
+
